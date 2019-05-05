276171e48a046057775dfb6786b80da6817027e1
Naresh Gurbuxani <naresh_gurbuxani@hotmail.com>
2019-05-04 23:09:50 -0400

    Document includes figure 1.2, which is boxplots of S&P 500

--- code/chap1/saveWageData.R
+++ code/chap1/saveWageData.R
@@ -1,4 +1,8 @@
 ## From ISLR package, save Wage data as csv file
 
 data(Wage, package = 'ISLR')
-write.csv(Wage, file = '../../data/Wage.csv')
+write.csv(Wage, file = 'data/Wage.csv')
+
+## Save Smarket data as csv file
+data(Smarket, package = 'ISLR')
+write.csv(Smarket, file = 'data/Smarket.csv')
