7c3130cbf28c6e80b0b6feda43ac63b35568d64f
Naresh Gurbuxani <naresh_gurbuxani@hotmail.com>
2019-05-07 13:03:27 -0400

    Refactored code calls function to draw 3d graph

--- code/chap2/incomeEdSeniority2_4.py
+++ code/chap2/incomeEdSeniority2_4.py
@@ -1,44 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from mpl_toolkits.mplot3d import Axes3D
-from statsmodels.formula.api import ols
+import incEdSen3d
 
-income_data = pd.read_csv('data/Income2.csv', index_col=0)
-
-reg = ols(formula='Income ~ Education + Seniority', data=income_data)
-reg_fit = reg.fit()
-income_data['income_predict'] = reg_fit.predict()
-
-ed_rng = np.linspace(income_data['Education'].min(),
-                     income_data['Education'].max())
-sen_rng = np.linspace(income_data['Seniority'].min(),
-                      income_data['Seniority'].max())
-ed, sen = np.meshgrid(ed_rng, sen_rng)
-ed_array = np.reshape(ed, newshape=-1)
-sen_array = np.reshape(sen, newshape=-1)
-income_fit = reg_fit.predict(exog=dict(Education=ed_array,
-                                       Seniority=sen_array))
-income_fit = np.reshape(income_fit, newshape=ed.shape)
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_wireframe(ed, sen, income_fit, alpha = 0.5, rstride=5, cstride=5)
-ax.set_xticklabels('')
-ax.set_yticklabels('')
-ax.set_zticklabels('')
-ax.set_xlabel(r'$\overrightarrow{Years of Education}$')
-ax.set_ylabel(r'$\overrightarrow{Seniority}$')
-ax.set_zlabel(r'$\overrightarrow{Income}$')
-
-ax.scatter(xs=income_data['Education'], ys=income_data['Seniority'],
-           zs=income_data['Income'], c='r')
-
-for i in range(income_data.shape[0]):
-    x = income_data.iloc[i]['Education']
-    y = income_data.iloc[i]['Seniority']
-    z1 = income_data.iloc[i]['income_predict']
-    z2 = income_data.iloc[i]['Income']
-    ax.plot([x, x], [y, y], [z1, z2], c='b', alpha=0.5)
-
-fig.tight_layout()
+my_formula = 'Income ~ Education + Seniority'
+incEdSen3d.plotIncomeEdSeniority(my_formula)
