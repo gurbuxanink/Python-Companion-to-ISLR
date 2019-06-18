3356c010564f00e1658a89eb9a427acefa357fa6
Naresh Gurbuxani <naresh_gurbuxani@hotmail.com>
2019-06-18 08:36:51 -0400

    Include index name as first column heading

--- code/general/myFuncs.py
+++ code/general/myFuncs.py
@@ -1,6 +1,7 @@
 # Functions used in various parts of code
 import pandas as pd
 
+
 def dfToList(my_df, hline_after=[0]):
     '''Assumes my_df is a pandas dataframe
     Outputs a list of lists suitable for printing as org-mode table'''
@@ -8,7 +9,7 @@ def dfToList(my_df, hline_after=[0]):
     # Each list within list will be a row in the table
     # Header needs an extra column for index names
     res_headers = list(my_df.columns)
-    res_headers.insert(0, '')
+    res_headers.insert(0, my_df.index.name)
 
     res_list = my_df.values.tolist()
     for i in range(len(res_list)):
