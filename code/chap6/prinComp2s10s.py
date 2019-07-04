81317002b760656324a69b098f4817124d39aa89
Naresh Gurbuxani <naresh_gurbuxani@hotmail.com>
2019-07-04 18:26:40 -0400

    Minor correction to recognize that second column (10 YR) is on x-axis

--- code/chap6/prinComp2s10s.py
+++ code/chap6/prinComp2s10s.py
@@ -3,7 +3,6 @@
 # We use 2-year and 10-year treasury yield history
 
 import matplotlib.pyplot as plt
-# from statsmodels import datasets
 import numpy as np
 from datetime import datetime
 import pandas as pd
@@ -15,7 +14,7 @@ treasury.set_index('Date', inplace=True)
 treasury_diff = treasury.diff(periods=-1) * 100  # basis points conversion
 treasury_2s10s = treasury_diff[['2 YR', '10 YR']].iloc[:-1]  # drop na
 mean_2s10s = treasury_2s10s.mean()
-# treasury_2s10s = treasury_2s10s - mean_2s10s
+treasury_2s10s = treasury_2s10s - mean_2s10s
 
 eigval, eigvec = np.linalg.eig(np.cov(treasury_2s10s, rowvar=False))
 pc1 = eigvec[:, 1]
@@ -24,7 +23,8 @@ pc1_2s10s = np.dot(treasury_2s10s, pc1)
 recover_2s10s_pc1 = np.dot(pc1_2s10s[:, np.newaxis], pc1[:, np.newaxis].T)
 pc2_2s10s = np.dot(treasury_2s10s, pc2)
 recover_2s10s_pc2 = np.dot(pc2_2s10s[:, np.newaxis], pc2[:, np.newaxis].T)
-# recover_2s10s_all = recover_2s10s_pc1 + recover_2s10s_pc2
+recover_2s10s_all = recover_2s10s_pc1 + \
+    recover_2s10s_pc2 + np.array(mean_2s10s)[np.newaxis, :]
 
 
 fig = plt.figure(figsize=(6, 6))
@@ -32,10 +32,10 @@ ax = fig.add_subplot(111)
 
 treasury_diff.plot(x='10 YR', y='2 YR', kind='scatter',
                    c='r', ax=ax, alpha=0.7)
-ax.plot(recover_2s10s_pc1[:, 0], recover_2s10s_pc1[:, 1], c='g', label='pc1',
+ax.plot(recover_2s10s_pc1[:, 1], recover_2s10s_pc1[:, 0], c='g', label='pc1',
         linewidth=1)
-ax.plot(recover_2s10s_pc2[:, 0], recover_2s10s_pc2[:, 1], c='b',
+ax.plot(recover_2s10s_pc2[:, 1], recover_2s10s_pc2[:, 0], c='b',
         linestyle=':', label='pc2')
-# ax.scatter(recover_2s10s_all[:, 1], recover_2s10s_all[:, 0], c='r', marker='x',
+# ax.scatter(recover_2s10s_all[:, 1], recover_2s10s_all[:, 0], c='k', marker='x',
 #            alpha=0.7)
 ax.legend()
