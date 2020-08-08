f84ba1a86967673f15bd3fba6473edfa76778ace
Naresh Gurbuxani <naresh_gurbuxani@hotmail.com>
2020-08-08 19:12:49 -0400

    Add code to plot figure 10.3

--- code/chap10/arrest_pca.py
+++ code/chap10/arrest_pca.py
@@ -1,5 +1,5 @@
 # arrest_pca.py
-# Code to create table 10.1
+# Code to create table 10.1, figures 10.1 and 10.3
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -37,3 +37,39 @@ for state in us_arrests_transformed.index:
 ax.set(xlim=[-3, 3], ylim=[-3, 3], xlabel='First Principal Component',
        ylabel='Second Principal Component')
 ax.axis('equal')
+
+# PCA without normalizing data
+pca_raw = PCA(n_components=2)
+pca_raw.fit(us_arrests)
+pca_raw_res = pd.DataFrame(pca_raw.components_.T, index=us_arrests.columns,
+                           columns=['PC1', 'PC2'])
+
+arrests_transformed_raw = pd.DataFrame(
+    pca_raw.transform(us_arrests), index=us_arrests.index,
+    columns=['pc1', 'pc2'])
+
+fig_raw = plt.figure(figsize=(8, 4))
+ax1 = fig_raw.add_subplot(1, 2, 1)
+for crime in pca_res.index:
+    pc1, pc2 = pca_res.loc[crime]
+    ax1.annotate(crime, xy=[0, 0], xytext=[pc1 * 2.5, pc2 * 2.5],
+                 arrowprops=dict(arrowstyle='<-', color='red', alpha=0.7),
+                 color='red', alpha=0.7, zorder=3)
+us_arrests_transformed.plot(x='pc1', y='pc2', kind='scatter', marker='.',
+                            alpha=0.7, ax=ax1)
+ax1.set(title='Scaled')
+
+ax2 = fig_raw.add_subplot(1, 2, 2)
+for crime in pca_raw_res.index:
+    pc1, pc2 = pca_raw_res.loc[crime]
+    ax2.annotate(crime, xy=[0, 0], xytext=[pc1 * 100, pc2 * 100],
+                 arrowprops=dict(arrowstyle='<-', color='red', alpha=0.7),
+                 color='red', alpha=0.7, zorder=3)
+arrests_transformed_raw.plot(x='pc1', y='pc2', kind='scatter', marker='.',
+                             alpha=0.7, ax=ax2)
+ax2.set(title='Unscaled')
+
+for axi in fig_raw.axes:
+    axi.set(xlabel='First Component', ylabel='Second Component')
+    axi.axis('equal')
+fig_raw.tight_layout()
