24c9848162011eac0a9aef2895e05b09cf847234
Naresh Gurbuxani <naresh_gurbuxani@hotmail.com>
2020-08-14 07:15:47 -0400

    Code to plot figure 10.9

--- code/chap10/dendrogram.py
+++ code/chap10/dendrogram.py
@@ -12,7 +12,21 @@ X[30:, 1] += 3
 y = np.concatenate([np.zeros(15), np.ones(15), np.ones(15) * 2])
 y = y.astype(int)
 
+# Plot figure 10.8
 fig, ax = plt.subplots()
 ax.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.get_cmap('viridis', 3),
            alpha=0.7)
 ax.set(xlabel=r'$X_1$', ylabel=r'$X_2$')
+
+# Plot figure 10.9
+Z = linkage(X, method='ward')
+
+fig_dendro = plt.figure(figsize=(8, 3))
+
+for i, thresh in enumerate([21, 15, 10]):
+    ax = fig_dendro.add_subplot(1, 3, i + 1)
+    dendrogram(Z, color_threshold=thresh, no_labels=True, ax=ax)
+    ax.axhline(y=thresh, linestyle='--', color='black', alpha=0.7)
+    ax.set(ylim=[0, 20])
+
+fig_dendro.tight_layout()
