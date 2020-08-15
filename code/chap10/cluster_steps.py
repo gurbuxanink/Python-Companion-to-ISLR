5b3f265ad5edbd2488527e06127f97838aaa414c
Naresh Gurbuxani <naresh_gurbuxani@hotmail.com>
2020-08-15 15:27:40 -0400

    Add code to plot figure 10.11

--- code/chap10/cluster_steps.py
+++ code/chap10/cluster_steps.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
+import pdb
 
 np.random.seed(4)
 X = np.random.normal(size=(9, 2))
@@ -25,3 +26,34 @@ ax2.set(xlim=[x1_min - 0.2, x1_max + 0.2], ylim=[x2_min - 0.2, x2_max + 0.2],
         xlabel=r'$X_1$', ylabel=r'$X_2$')
 
 fig.tight_layout()
+
+
+# Figure 10.11
+def plot_Z(z_ind, Z, X, ax, col='gray'):
+    '''Plot z_ind data from X'''
+    x_ind = Z[z_ind, :2].astype(int)
+    for obs in x_ind:
+        if obs >= X.shape[0]:
+            obs -= X.shape[0]
+            plot_Z(obs, Z, X, ax, col=col)
+        else:
+            ax.text(X[obs, 0], X[obs, 1], str(y[obs]), ha='center',
+                    va='center', bbox=dict(facecolor=col, alpha=0.2),
+                    zorder=1)
+
+
+fig_steps, ax = plt.subplots(2, 2)
+
+for axi in ax.flatten():
+    for i, s in enumerate(y):
+        axi.text(X[i, 0], X[i, 1], str(s), ha='center', va='center')
+        axi.set(xlim=[x1_min - 0.2, x1_max + 0.2],
+                ylim=[x2_min - 0.2, x2_max + 0.2],
+                xlabel=r'$X_1$', ylabel=r'$X_2$')
+
+my_colors = ['red', 'green', 'blue']
+for i, axi in enumerate(ax.flatten()):
+    for j in range(i):
+        plot_Z(j, Z, X, axi, col=my_colors[j])
+
+fig_steps.tight_layout()
